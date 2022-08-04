aa927f81302bf9afaec9ad733e53aecec3e3472c
Jules Maestro <jlsodai@gmail.com>
2022-08-04 13:36:37 +0000

    remove unique constraints on address and phone number

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,8 +23,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->uuid('avatar')->nullable();
-            $table->string('address')->unique();
-            $table->string('phone_number')->unique();
+            $table->string('address');
+            $table->string('phone_number');
             $table->boolean('is_marketing')->default(0);
             $table->rememberToken();
             $table->timestamps();
