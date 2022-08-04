4d87507420750b22099ddebe038bb6d3e42d68b4
Jules Maestro <jlsodai@gmail.com>
2022-08-04 11:45:06 +0000

    create v1 Users feature and unit test files

---
+++ tests/Unit/v1/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\v1;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
