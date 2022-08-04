6a72dbae4dd525663512f4db3b5b58dc8f5440fc
Jules Maestro <jlsodai@gmail.com>
2022-08-04 11:51:28 +0000

    add tests to check if tables and required columns exist

--- tests/Feature/v1/UsersTest.php
+++ tests/Feature/v1/UsersTest.php
@@ -4,19 +4,20 @@ namespace Tests\Feature\v1;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 class UsersTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_users_table_exists()
+    {
+        $this->assertTrue(Schema::hasTable('users'));
+    }
+
+    public function test_columns_exist()
+    {
+        $this->assertTrue(Schema::hasColumns('users', ['id', 'uuid', 'first_name', 'last_name', 'is_admin', 'email', 'email_verified_at', 'password', 'avatar', 'address', 'phone_number', 'is_marketing', 'created_at', 'updated_at', 'last_login_at']));
     }
 }
