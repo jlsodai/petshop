5d0b19e3e260dd9d6fa49afa99d6eda8b9a4f17a
Jules Maestro <jlsodai@gmail.com>
2022-08-04 12:48:36 +0000

    create api v1 route file and base url

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/*
+ * API version 1 route
+ */
+Route::group(['prefix' => 'v1', 'name' => 'v1.'], function() {
+    require base_path('routes/api_v1.php');
 });
