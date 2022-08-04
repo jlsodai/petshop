791cbbc27d7c28e72624277fdcd66b5ff6d44dfe
Jules Maestro <jlsodai@gmail.com>
2022-08-04 11:52:11 +0000

    add required columns to users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->uuid('uuid');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->uuid('avatar')->nullable();
+            $table->string('address')->unique();
+            $table->string('phone_number')->unique();
+            $table->boolean('is_marketing')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
