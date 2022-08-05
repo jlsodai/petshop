5f7ef46dec159753cb827a4e8aaee2a9cf7405e9
Jules Maestro <jlsodai@gmail.com>
2022-08-05 08:18:21 +0000

    add payment routes... refactor code

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -6,15 +6,32 @@ Route::get('/', function () {
     return response(['status' => 'success', 'api_version' => 1]);
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Api\V1'], function() {
-    /*
-     * Auth Routes
-     */
-    Route::controller(AuthController::class)->prefix('user')->as('v1.user.')->group(function () {
-        Route::get('/', 'show')->name('show');
-        Route::delete('/', 'delete')->name('delete');
-        Route::post('create', 'create')->name('create');
-        Route::post('login', 'login')->name('login');
-        Route::post('logout', 'logout')->name('logout');
-    });
+/*
+ * Auth Routes
+ */
+Route::group([
+    'controller' => AuthController::class,
+    'prefix' => 'user',
+    'as' => 'user.',
+], function() {
+    Route::get('/', 'show')->name('show');
+    Route::delete('/', 'delete')->name('delete');
+    Route::post('create', 'create')->name('create');
+    Route::post('login', 'login')->name('login');
+    Route::post('logout', 'logout')->name('logout');
+});
+
+/*
+ * Payment Routes
+ */
+Route::group([
+    'controller' => PaymentController::class,
+    'prefix' => 'payments',
+    'as' => 'payments.'
+], function() {
+    Route::get('/', 'index')->name('index');
+    Route::post('/create', 'create')->name('create');
+    Route::get('/{payment:uuid}', 'show')->name('show');
+    Route::put('/{payment:uuid}', 'update')->name('update');
+    Route::delete('/{payment:uuid}', 'delete')->name('delete');
 });
