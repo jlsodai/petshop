9e38f1d6e19d10faf5dbf92faa7b382f64cb2d16
Jules Maestro <jlsodai@gmail.com>
2022-08-05 04:23:16 +0000

    add user auth routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response(['status' => 'success', 'api_version' => 1]);
 });
+
+Route::group(['namespace' => 'App\Http\Controllers\Api\V1'], function() {
+    /*
+     * Auth Routes
+     */
+    Route::controller(AuthController::class)->prefix('user')->as('v1.user.')->group(function () {
+        Route::get('/', 'show')->name('show');
+        Route::delete('/', 'delete')->name('delete');
+        Route::post('create', 'create')->name('create');
+        Route::post('login', 'login')->name('login');
+        Route::post('logout', 'logout')->name('logout');
+    });
+});
