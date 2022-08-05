8c116daf564f6cd88009c88ae3536dfd90fbbba6
Jules Maestro <jlsodai@gmail.com>
2022-08-05 04:23:38 +0000

    create UserRequest validation

---
+++ app/Http/Requests/v1/UserRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\v1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ];
+    }
+}
