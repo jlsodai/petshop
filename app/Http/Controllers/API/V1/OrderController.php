0c558f7c079e0a3bdd7674902f43c08402549f45
Jules Maestro <jlsodai@gmail.com>
2022-08-06 20:54:28 +0000

    implement order payment to generate checkout url pending creation of stripe package

--- app/Http/Controllers/API/V1/OrderController.php
+++ app/Http/Controllers/API/V1/OrderController.php
@@ -9,6 +9,7 @@ use App\Models\OrderStatus;
 use App\Models\Payment;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Stripe\Stripe;
 
 class OrderController extends Controller
 {
@@ -36,5 +37,41 @@ class OrderController extends Controller
         // Generate Stripe Checkout XXX
 
         return response($order);
+    }
+
+    public function payment(Order $order)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $base_url = env('APP_URL') . '/payment/' . $order->uuid;
+        $line_items = [];
+
+
+        foreach ($order->products as $product) {
+
+            $product_model = Product::where('uuid', $product['uuid'])->first();
+
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => $product_model->title,
+                    ],
+                    'unit_amount' => (int) $product_model->price * 100,
+                ],
+                'quantity' => 1,
+            ];
+        }
+
+        $checkout_session = \Stripe\Checkout\Session::create([
+            'line_items' => [$line_items],
+            'mode' => 'payment',
+            'success_url' => $base_url . '/?status=success&gtw=stripe',
+            'cancel_url' => $base_url . '/?status=failure&gtw=stripe',
+        ]);
+
+        return response([
+            "checkout_url" => $checkout_session->url
+        ]);
     }
 }
