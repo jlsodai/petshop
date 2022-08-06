84c29fb1ada77b836784197d2ac203fca33aecaa
Jules Maestro <jlsodai@gmail.com>
2022-08-06 18:21:22 +0000

    add constrains on orders table

--- database/migrations/2022_08_06_154314_create_orders_table.php
+++ database/migrations/2022_08_06_154314_create_orders_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->onDelete('cascade');
-            $table->foreignId('order_status_id')->onDelete('cascade');
-            $table->foreignId('payment_id')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('order_status_id')->constrained('order_statuses')->onDelete('cascade');
+            $table->foreignId('payment_id')->constrained('payments')->onDelete('cascade');
             $table->uuid('uuid');
             $table->json('products');
             $table->json('address');
-            $table->float('delivery_fee', 8, 2);
+            $table->float('delivery_fee', 8, 2)->nullable();
             $table->float('amount', 8, 2);
             $table->timestamps();
             $table->timestamp('shipped_at')->nullable();
