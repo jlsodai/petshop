3dd3a2a4dc00a0696ae95269cff7b0b33d4051ad
Jules Maestro <jlsodai@gmail.com>
2022-08-06 11:16:09 +0000

    add CurrencyConvert Service Provider

---
+++ packages/julius/currency-convert/src/CurrencyConvertServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Julius\CurrencyConvert;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurrencyConvertServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
