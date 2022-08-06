24dad977d9beedeb108f8f8a4dc7aff7faf1041c
Jules Maestro <jlsodai@gmail.com>
2022-08-06 16:35:53 +0000

    add timestamps to products migration

---
+++ database/migrations/2022_08_06_162014_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->foreignUuid('category_uuid')->onDelete('cascade');
+            $table->string('title');
+            $table->float('price', 8, 2);
+            $table->text('description');
+            $table->json('metadata');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
