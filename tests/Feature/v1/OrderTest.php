770765eb3d0b1645412cb23ad094488cb20bc237
Jules Maestro <jlsodai@gmail.com>
2022-08-06 15:35:30 +0000

    add OrderTest

---
+++ tests/Feature/v1/OrderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\v1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    //
+}
