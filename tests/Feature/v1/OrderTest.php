97b318c2921a7d333dd5fbaacf6701ff731c2498
Jules Maestro <jlsodai@gmail.com>
2022-08-06 15:38:46 +0000

    include test for order routes

--- tests/Feature/v1/OrderTest.php
+++ tests/Feature/v1/OrderTest.php
@@ -4,9 +4,20 @@ namespace Tests\Feature\v1;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
 class OrderTest extends TestCase
 {
-    //
+    public function test_order_routes_exist()
+    {
+        $this->assertTrue(Route::has('v1.orders.index'));
+        $this->assertTrue(Route::has('v1.orders.create'));
+        $this->assertTrue(Route::has('v1.orders.show'));
+        $this->assertTrue(Route::has('v1.orders.update'));
+        $this->assertTrue(Route::has('v1.orders.delete'));
+        $this->assertTrue(Route::has('v1.orders.download'));
+        $this->assertTrue(Route::has('v1.orders.dashboard'));
+        $this->assertTrue(Route::has('v1.orders.shipmentLocator'));
+    }
 }
