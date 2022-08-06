ce4020bad89f65ddd7a3fbb6004f2e383dd62270
Jules Maestro <jlsodai@gmail.com>
2022-08-06 15:27:06 +0000

    stripe test complete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Stripe\Stripe;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('stripetest', function() {
+    Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+    $base_url = 'http://petshop.devt/api/v1/payment/123123123123123123/';
+
+    $checkout_session = \Stripe\Checkout\Session::create([
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'usd',
+                'product_data' => [
+                    'name' => 'T-shirt',
+                ],
+                'unit_amount' => 2000,
+            ],
+            'quantity' => 1,
+        ]],
+        'mode' => 'payment',
+        'success_url' => $base_url . '?status=success&gtw=stripe',
+        'cancel_url' => $base_url . '?status=failure&gtw=stripe',
+    ]);
+
+    return redirect($checkout_session->url);
 });
