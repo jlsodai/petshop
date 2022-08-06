3c248b5d62af2618d66f626be9848382adaf16c5
Jules Maestro <jlsodai@gmail.com>
2022-08-06 20:52:17 +0000

    remove stripetest route

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,3 @@ use Stripe\Stripe;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('stripetest', function() {
-    Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
-
-    $base_url = 'http://petshop.devt/api/v1/payment/123123123123123123/';
-
-    $checkout_session = \Stripe\Checkout\Session::create([
-        'line_items' => [[
-            'price_data' => [
-                'currency' => 'usd',
-                'product_data' => [
-                    'name' => 'T-shirt',
-                ],
-                'unit_amount' => 2000,
-            ],
-            'quantity' => 1,
-        ]],
-        'mode' => 'payment',
-        'success_url' => $base_url . '?status=success&gtw=stripe',
-        'cancel_url' => $base_url . '?status=failure&gtw=stripe',
-    ]);
-
-    return redirect($checkout_session->url);
-});
