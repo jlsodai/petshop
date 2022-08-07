5ba6d6ba1c6bc3ff6626bb01554f8f7dce32efd3
Jules Maestro <jlsodai@gmail.com>
2022-08-07 17:03:10 +0000

    add swagger base annotation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="PetShop API Documentation",
+ *    version="1.0.0",
+ * )
+ * 
+ * @OA\Get(
+ *     path="/",
+ *     description="Home page",
+ *     @OA\Response(response="default", description="Welcome page")
+ * )
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
